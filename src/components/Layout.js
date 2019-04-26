37878be362e063e8e792fcd8144cc42bd8e3fbfb
Thomas Malt <thomas@malt.no>
2019-04-26 18:03:33 +0200

    added metatag for og:image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,6 +12,10 @@ const Layout = ({ children }) => {
           
           Planleggingen av programmet for 2019-2020 er i gang, og mer informasjon vil bli lagt ut så snart det er klart."
         />
+        <meta
+          property="og:image"
+          content="https://academy.knowit.no/assets/knowit_academy_banner_green.png"
+        />
         <link rel="canonical" href="https://academy.knowit.no/" />
         <html lang="nb" />
       </Helmet>
