9384903f66eedaf39ca40a5ada239eb039b8e5e1
Thomas Malt <thomas@malt.no>
2019-04-27 14:23:01 +0200

    implemented parsing of markdown and creating pages

---
+++ gatsby-node.js
@@ -0,0 +1,36 @@
+const path = require("path")
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const programsTemplate = path.resolve(`src/templates/programsTemplate.js`)
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: programsTemplate,
+        context: {}, // additional data can be passed via context
+      })
+    })
+  })
+}
