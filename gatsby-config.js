40af8e83792b0266de959f56e19fcd0d809ec8f1
Thomas Malt <thomas@malt.no>
2019-04-27 14:21:48 +0200

    added support for markdown pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@ module.exports = {
       "Knowit Academy Norway er Knowits kompetanseutviklingsprogram for ansatte i alle norske enheter. Planleggingen av programmet for 2019-2020 er i gang, og mer informasjon vil bli lagt ut så snart det er klart.",
     ogImage: "https://academy.knowit.no/assets/knowit_academy_banner_green.png",
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "markdown-pages",
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
 }
