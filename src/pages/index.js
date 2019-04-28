3785863cd7c1787bcf9e7c149674fe7b1c4a2bb3
Thomas Malt <thomas@malt.no>
2019-04-28 23:59:13 +0200

    Added graphql query to fetch data about all the markdown pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,20 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/FrontLayout"
 import AboutAcademy from "../components/AboutAcademy"
+import ThePrograms from "../components/ThePrograms"
 
 const Page = ({ data }) => {
   const { siteMetadata } = data.site
+
+  // remove unneccessary complexity from the data.
+  const programPages = data.allMarkdownRemark.edges
+    .filter(i => i.node.frontmatter.path.match(/^\/programs/))
+    .map(item => item.node.frontmatter)
+
   return (
     <Layout data={siteMetadata}>
       <AboutAcademy />
+      <ThePrograms pages={programPages} />
     </Layout>
   )
 }
@@ -22,6 +30,18 @@ export const query = graphql`
         siteUrl
         description
         ogImage
+      }
+    }
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+            description
+          }
+        }
       }
     }
   }
