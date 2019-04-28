7ee5cb4f3cb08b183d08564f2c8b79a48f188c4b
Thomas Malt <thomas@malt.no>
2019-04-28 12:53:18 +0200

    created a shared Helmet component

---
+++ src/components/MainHelmet.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Helmet from "react-helmet"
+
+const MainHelmet = ({ metadata }) => (
+  <Helmet>
+    <title>{metadata.title}</title>
+    <meta name="description" content={metadata.description} />
+    <meta
+      name="keywords"
+      content="Knowit, Knowit Academy, Knowit Academy Norway, Recruitment, Personal Development, Career Path"
+    />
+    <meta property="og:title" content={metadata.title} />
+    <meta property="og:description" content={metadata.description} />
+    <meta property="og:url" content={metadata.siteUrl} />
+    <meta property="og:image" content={metadata.ogImage} />
+    <link rel="canonical" href="https://academy.knowit.no/" />
+    <html lang="nb" />
+  </Helmet>
+)
+
+export default MainHelmet
