3667c8a108de40343a7efd7e7d86b5a25a2d7108
Thomas Malt <thomas@malt.no>
2019-04-28 23:56:14 +0200

    Created a first version of a Knowit Academy footer

--- src/components/AcademyFooter.js
+++ src/components/AcademyFooter.js
@@ -1,42 +1,69 @@
-import React from "React"
+import React from "react"
 import styled from "styled-components"
 import colors from "../utils/colors.js"
 
-const AFooter = styled.footer`
-  position: fixed;
-  bottom: 0px;
+const Wrapper = styled.div`
+  // position: fixed;
   display: flex;
-  background-color: ${colors.knowit.blue[3]};
-  border-top: 2px solid ${colors.knowit.blue[2]};
+  justify-content: center;
+  // bottom: 0px;
   width: 100vw;
-  // height: 48px;
-  padding: 6px 1.4em 6px 1.4em;
+  padding: 0;
+  margin: auto;
+  background-color: ${colors.knowit.green[3]};
+  border-top: 1px solid ${colors.knowit.green[1]};
+`
+const Footer = styled.footer`
+  display: flex;
+  padding: 12px 1.4rem;
+  align-items: center;
+  max-width: 800px;
+  width: 800px;
+
+  & > *:not(:first-child) {
+    margin-left: 0.8rem;
+  }
 
   div {
-    margin: 12px 12px 12px 1.4em;
+    display: inline-block;
     font-size: calc(0.2em + 0.6em);
     font-weight: 200;
-    color: ${colors.knowit.blue[0]};
+    color: black;
   }
 
   img {
     width: auto;
-    height: 42px;
+    height: 56px;
+  }
+
+  a {
+    color: ${colors.knowit.green[0]};
+    font-weight: 600;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
   }
 `
 
 const AcademyFooter = () => {
   return (
-    <AFooter>
-      <div>
-        Har du spørsmål angående Knowit Academy? Ta kontakt på{" "}
-        <a href="mailto:academy@knowit.no">academy@knowit.no</a>
-      </div>
-      <img
-        src="/assets/knowit_academy_box_logo.png"
-        alt="Knowit Academy Logo"
-      />
-    </AFooter>
+    <Wrapper>
+      <Footer>
+        <a href="/">
+          <img
+            src="/assets/knowit_academy_box_logo.png"
+            alt="Knowit Academy Logo"
+          />
+        </a>
+        <div>
+          Har du spørsmål om Knowit Academy? Send en epost til{" "}
+          <a href="mailto:academy@knowit.no">academy@knowit.no</a> så svarer vi
+          så snart vi rekker.
+        </div>
+      </Footer>
+    </Wrapper>
   )
 }
 
