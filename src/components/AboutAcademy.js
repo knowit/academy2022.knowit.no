14b43c27a311d61d7fb3809ec6e20cb7394745a8
Thomas Malt <thomas@malt.no>
2019-04-28 12:51:36 +0200

    Added a heading to about academy to make it more sellable

--- src/components/AboutAcademy.js
+++ src/components/AboutAcademy.js
@@ -1,34 +1,41 @@
 import React from "react"
 import styled from "styled-components"
+import colors from "../utils/colors"
 
-const AboutDiv = styled.div`
-  display: flex;
+const AboutSection = styled.section`
   margin: auto;
   background-color: white;
   max-width: 800px;
   padding: 1vw 1.4em;
   hyphens: auto;
+
+  h2 {
+    font-weight: 600;
+    color: ${colors.knowit.blue[1]};
+  }
 `
 
-class AboutAcademy extends React.Component {
-  render() {
-    return (
-      <AboutDiv>
-        <p>
-          Knowit Academy Norway er Knowits norske kompetanseutviklingsprogram.
-          Målgruppen for programmet er ansatte i alle Knowits selskaper i Norge.
-        </p>
-        <p>
-          For 2018-2019 bestod Knowit Academy av følgende program: Team
-          Lead-skolen, Tech Lead-skolen og Prosjektleder-skolen.
-        </p>
-        <p>
-          Planlegging av programmet for 2019-2020 er i gang, og mer informasjon
-          vil bli lagt ut så snart det er klart.
-        </p>
-      </AboutDiv>
-    )
-  }
-}
+//class AboutAcademy extends React.Component {
+//  render() {
+//     return (
+const AboutAcademy = () => (
+  <AboutSection>
+    <h2>Norges beste kompetanseutviklingsprogram</h2>
+    <p>
+      Knowit Academy Norway er Knowits norske kompetanseutviklingsprogram.
+      Målgruppen for programmet er ansatte i alle Knowits selskaper i Norge. Vår
+      ambisjon er å lage et kompetanseutviklingsprogram som er bedre enn noe
+      annet du kan finne.
+    </p>
+    <p>
+      For 2018-2019 bestod Knowit Academy av følgende program: Team Lead-skolen,
+      Tech Lead-skolen og Prosjektleder-skolen.
+    </p>
+    <p>
+      Planlegging av programmet for 2019-2020 er i gang, og mer informasjon vil
+      bli lagt ut så snart det er klart.
+    </p>
+  </AboutSection>
+)
 
 export default AboutAcademy
