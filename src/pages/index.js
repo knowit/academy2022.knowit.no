5b2611eebe0be2acb2db1e1a21130bec411da17a
Thomas Malt <thomas@malt.no>
2019-06-05 14:36:36 +0200

    Added signup information stub

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,46 @@ import { graphql } from "gatsby"
 import Layout from "../components/FrontLayout"
 import AboutAcademy from "../components/AboutAcademy"
 import ThePrograms from "../components/ThePrograms"
+import styled from "styled-components"
+import colors from "../utils/colors"
+
+const SignUpTitle = styled.h2`
+  color: ${colors.knowit.purple[0]};
+`
+
+const SignUpButton = styled.p`
+  text-align: center;
+  a {
+    display: inline-block;
+    text-decoration: none;
+    background-color: ${colors.knowit.green[1]};
+    height: 24px;
+    padding: 16px 16px;
+    color: white;
+    border-radius: 4px;
+    width: 320px;
+    vertical-align: middle;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.24);
+  }
+`
+
+const SignUp = () => {
+  return (
+    <section id="signup">
+      <SignUpTitle>Påmelding</SignUpTitle>
+      <p>
+        Påmelding er planlagt å starte i midten av august, med{" "}
+        <b>søknadsfrist 31. august, 2019</b>.
+      </p>
+      <p>Mer informasjon om dette vil bli sendt ut rett over sommeren.</p>
+      <SignUpButton>
+        <a href="how-to-apply" alt="Information about how to apply">
+          Hvordan søke
+        </a>
+      </SignUpButton>
+    </section>
+  )
+}
 
 const Page = ({ data }) => {
   const { siteMetadata } = data.site
@@ -20,6 +60,7 @@ const Page = ({ data }) => {
     <Layout data={siteMetadata}>
       <AboutAcademy content={aboutPage} />
       <ThePrograms pages={programPages} />
+      <SignUp />
     </Layout>
   )
 }
