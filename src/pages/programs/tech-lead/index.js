b9737d7f497b10ef0ae6a178e8b0a0366db7512c
Thomas Malt <thomas@malt.no>
2019-06-12 12:57:03 +0200

    added start index page for tech-lead programme

---
+++ src/pages/programs/tech-lead/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+const TechLeadProgram = ({ data }) => {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  )
+}
+
+export default TechLeadProgram
