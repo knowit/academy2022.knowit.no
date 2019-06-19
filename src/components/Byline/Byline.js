2ae3d1a874f8263ac0ff45c6ac77b32d9133329c
Thomas Malt <thomas@malt.no>
2019-06-19 19:33:06 +0200

    Added lambda to use default email if missing from frontmatter

--- src/components/Byline/Byline.js
+++ src/components/Byline/Byline.js
@@ -2,11 +2,14 @@ import React from "react"
 import css from "./Byline.module.scss"
 
 const Byline = ({ title, author, email, updated }) => {
+  const mailto = (email =>
+    email === null ? "mailto:academy@knowit.no" : `mailto:${email}`)(email)
+
   return (
     <>
       <h1>{title}</h1>
       <div className={css.byline}>
-        Sist oppdatert {updated} av <a href={`mailto:${email}`}>{author}</a>
+        Sist oppdatert {updated} av <a href={mailto}>{author}</a>
       </div>
     </>
   )
