45c23713768837beb4bccf78a6a5a1951e2db1bd
Thomas Malt <thomas@malt.no>
2019-06-28 15:45:28 +0200

    Added a BlogHeader that is simpler. (for articles we write

---
+++ src/components/BlogHeader/BlogHeader.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Icon from "../Icon"
+import moment from "moment"
+import css from "./BlogHeader.module.scss"
+
+const BlogHeader = ({ data }) => {
+  const { title, author, email, updated } = data.frontmatter
+  const date = moment(updated).format("LL")
+  return (
+    <>
+      <h1>{title}</h1>
+      <div className={css.blogHeader}>
+        <Icon name="access_time" />
+        <span className={css.byline}>
+          Oppdatert {date} av <a href={`mailto:${email}`}>{author}</a>
+        </span>
+      </div>
+    </>
+  )
+}
+
+export default BlogHeader
