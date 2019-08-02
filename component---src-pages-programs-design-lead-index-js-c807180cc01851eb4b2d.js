b70a69c0fda7c6eaf439d0940a53a8c3937c2996
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-02 12:59:28 +0000

    Deploy knowit/academy.knowit.no to github.com/knowit/academy.knowit.no.git:gh-pages

--- component---src-pages-programs-design-lead-index-js-c807180cc01851eb4b2d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{169:function(n,e,a){"use strict";a.r(e);var r=a(0),t=a.n(r),o=a(190);e.default=function(){return t.a.createElement(o.a,{program:"design-lead"})}}}]);
-//# sourceMappingURL=component---src-pages-programs-design-lead-index-js-c807180cc01851eb4b2d.js.map
