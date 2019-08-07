e79da07272efefa8794ea67c9f3f3a37b327f8c3
Thomas Malt <thomas@malt.no>
2019-08-07 16:25:42 +0200

    fixed programs to use button to link to more information

--- src/components/ThePrograms/ThePrograms.js
+++ src/components/ThePrograms/ThePrograms.js
@@ -1,20 +1,34 @@
-import React from "react";
-import css from "./ThePrograms.module.scss";
+import React from "react"
+import Button from "../Button"
+import colors from "../../utils/colors"
+import css from "./ThePrograms.module.scss"
 
 const ThePrograms = ({ pages }) => {
-  const programPages = pages.map(item => {
-    const path = item.path.replace(/about-/, "");
+  const programPages = pages.map((item) => {
+    const path = item.path.replace(/about-/, "")
+    // <a href={path}></a>
     return (
       <li key={item.path}>
-        <a href={path}>
-          <div>
-            <h4>{item.title}</h4>
-            <p>{item.description}</p>
-          </div>
-        </a>
+        <div>
+          <h4>{item.title}</h4>
+          <p>{item.description}</p>
+          <Button
+            link={path}
+            alt="More information about the programs"
+            style={{
+              margin: "8px auto",
+              color: "white",
+              backgroundColor: colors.knowit.purple[1],
+              width: "80%",
+              maxWidth: "320px",
+            }}
+          >
+            Les mer
+          </Button>
+        </div>
       </li>
-    );
-  });
+    )
+  })
 
   return (
     <section id="theprograms" className={css.thePrograms}>
@@ -23,7 +37,7 @@ const ThePrograms = ({ pages }) => {
         <ul>{programPages}</ul>
       </nav>
     </section>
-  );
-};
+  )
+}
 
-export default ThePrograms;
+export default ThePrograms
