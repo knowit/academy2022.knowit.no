c55409504c602e58629fccb5e0e0789cf9018239
Thomas Malt <thomas@malt.no>
2019-08-21 15:59:56 +0200

    Updated header menu to new layout

---
+++ src/components/AcademyHeaderMenu/AcademyHeaderMenu.js
@@ -0,0 +1,31 @@
+import React from "react"
+import css from "./AcademyHeaderMenu.module.scss"
+
+const AcademyHeaderMenu = () => {
+  return (
+    <header className={css.academyFrontHeader}>
+      <div className={css.innerContainer}>
+        <div className={css.title}>Knowit Academy</div>
+        <div className={css.grower}></div>
+        <nav className={css.academyHeaderMenu}>
+          <ul>
+            <li>
+              <a href="/#about">Hjem</a>
+            </li>
+            <li>
+              <a href="/#theprograms">Programmene</a>
+            </li>
+            <li>
+              <a href="/application-form">Påmelding</a>
+            </li>
+            <li>
+              <a href="/#about">Om Knowit Academy</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default AcademyHeaderMenu
