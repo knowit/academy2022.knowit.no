c56a732eea87e98dcfe1f55d0e487620e8f377ed
Thomas Malt <thomas@malt.no>
2019-08-28 16:19:35 +0200

    adding email to site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "Knowit Academy Norway",
     siteUrl: "https://academy.knowit.no/",
+    email: "academy@knowit.no",
     description:
       "Knowit Academy Norway er Knowits kompetanseutviklingsprogram for ansatte i alle norske enheter. Planleggingen av programmet for 2019-2020 er i gang, og mer informasjon vil bli lagt ut så snart det er klart.",
     ogImage: "https://academy.knowit.no/assets/knowit_academy_banner_green.png",
