8883fedabd854b76cb702e555c638819ccd99139
Thomas Malt <thomas@malt.no>
2022-06-28 23:23:15 +0200

    endret filnavn for about

--- src/components/AboutAcademy/about-academy.js
+++ src/components/AboutAcademy/about-academy.js
@@ -1,20 +1,23 @@
 import * as React from 'react'
-import * as css from './about.module.scss'
+import * as css from './about-academy.module.scss'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 
 const AboutAcademy = ({ content }) => {
   return (
     <Container maxWidth="tablet">
-      <Box sx={{ textAlign: 'center', paddingBottom: 4 }}>
-        <Box sx={{ paddingTop: 12, paddingBottom: 4 }}>
+      <Box sx={{ paddingTop: 10, paddingBottom: 8 }}>
+        <Box sx={{ textAlign: 'center', paddingBottom: 2 }}>
           <img
             height="108"
             src="/icons/medal.svg"
             alt="Knowit Academy Medal Icon"
           />
         </Box>
-        <Box dangerouslySetInnerHTML={{ __html: content.html }} />
+        <Box
+          className={css.aboutSection}
+          dangerouslySetInnerHTML={{ __html: content.html }}
+        />
       </Box>
     </Container>
   )
