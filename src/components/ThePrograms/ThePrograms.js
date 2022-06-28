ca8c61498033683a71d943b0a5ed2c31d8a16e45
Thomas Malt <thomas@malt.no>
2022-06-28 23:20:10 +0200

    gikk over til material-ui

--- src/components/ThePrograms/ThePrograms.js
+++ src/components/ThePrograms/ThePrograms.js
@@ -1,34 +1,79 @@
 import * as React from 'react'
 import { useAboutPrograms } from 'hooks/useAboutPrograms'
-import * as css from './ThePrograms.module.scss'
+import {
+  CardActions,
+  CardContent,
+  Container,
+  Grid,
+  Button,
+  Typography,
+  Box,
+  Card,
+} from '@mui/material'
 
 const ThePrograms = () => {
   const programs = useAboutPrograms()
-  console.log('programs', programs)
-  const programPages = programs.map((item) => {
-    const path = item.path.replace(/about-/, '')
-    // <a href={path}></a>
-    return (
-      <li key={item.path}>
-        <a href={path}>
-          <h3>{item.title}</h3>
-          <p>{item.description}</p>
-          <img alt="" src="/icons/arrow.svg" width="28px" />
-        </a>
-      </li>
-    )
-  })
 
   return (
-    <section id="theprograms" className={css.thePrograms}>
-      <div className={css.gridContainer}>
-        <div className={css.frontContent}>
-          <h2>Programmene</h2>
-          <nav>
-            <ul>{programPages}</ul>
-          </nav>
-        </div>
-      </div>
+    <section id="theprograms">
+      <Container maxWidth="tablet">
+        <Box pt={8} pb={8}>
+          <Box pb={6}>
+            <Typography variant="h2" component="h2" align="center">
+              Programmene
+            </Typography>
+          </Box>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container rowSpacing={2} columnSpacing={6}>
+              {programs
+                .sort((a, b) => (a.title > b.title ? 1 : -1))
+                .map((item) => {
+                  const path = item.path.replace(/about-/, '')
+                  // <a href={path}>
+                  return (
+                    <Grid item tablet={6}>
+                      <Card
+                        variant="outlined"
+                        sx={{ height: '100%', border: 'none' }}
+                      >
+                        <a href={path}>
+                          <CardContent sx={{ padding: 0 }}>
+                            <Typography variant="h4" gutterBottom>
+                              {item.title}
+                            </Typography>
+                            <Typography variant="body1">
+                              {item.description}
+                            </Typography>
+                          </CardContent>
+                          <CardActions
+                            sx={{
+                              display: 'flex',
+                              justifyContent: 'flex-end',
+                            }}
+                          >
+                            <Button
+                              variant="text"
+                              sx={{ fontWeight: 'bold' }}
+                              endIcon={
+                                <img
+                                  alt="Dekorativ pil som peker til mer informasjon"
+                                  src="/icons/arrow.svg"
+                                  height="24px"
+                                />
+                              }
+                            >
+                              Les mer
+                            </Button>
+                          </CardActions>
+                        </a>
+                      </Card>
+                    </Grid>
+                  )
+                })}
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
     </section>
   )
 }
