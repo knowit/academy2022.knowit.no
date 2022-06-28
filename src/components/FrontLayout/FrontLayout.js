52b1c5e2a14242b818050fec19717edbe5ff6c4c
Thomas Malt <thomas@malt.no>
2022-06-28 23:21:47 +0200

    gikk over til muis css baseline

--- src/components/FrontLayout/FrontLayout.js
+++ src/components/FrontLayout/FrontLayout.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import * as React from 'react'
 import { MainHelmet } from '../MainHelmet'
 import HeaderMenu from '../AcademyHeaderMenu'
 import Footer from '../Footer/Footer'
+import { CssBaseline } from '@mui/material'
 
 const FrontLayout = ({ children, data, sitemeta }) => {
   return (
     <>
+      <CssBaseline />
       <MainHelmet metadata={data} />
       <HeaderMenu data={data} />
       <main>{children}</main>
