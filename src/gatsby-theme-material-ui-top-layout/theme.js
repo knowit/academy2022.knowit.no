5ea558c903b7e7ef7abfdfc4f4b7261b11f3324c
Thomas Malt <thomas@malt.no>
2022-06-28 10:43:01 +0200

    new theme based on material-ui

---
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -0,0 +1,46 @@
+import { createTheme } from '@mui/material/styles'
+import * as css from 'components/FrontLayout/FrontLayout.module.scss'
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 768,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
+  components: {
+    MuiToolbar: {
+      styleOverrides: {
+        root: {
+          height: 86,
+          minHeight: 86,
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        text: {
+          color: '#333333',
+        },
+      },
+    },
+  },
+  typography: {
+    fontFamily: 'arial',
+  },
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#b7debd',
+    },
+    secondary: {
+      main: '#fac0b1',
+    },
+    background: {
+      default: '#e4e1db',
+    },
+  },
+})
+
+export default theme
