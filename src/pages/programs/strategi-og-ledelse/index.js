a7b6c67af910a3144c38ad38532846e479cb9cc3
Thomas Malt <thomas@malt.no>
2022-06-28 15:40:33 +0200

    fjernet strategi og ledelse fra årets programmer

--- src/pages/programs/strategi-og-ledelse/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ProgramPage from 'components/ProgramPage/programpage'
-
-const PersonalledelseProgram = () => {
-  return <ProgramPage program="strategi-og-ledelse" />
-}
-
-export default PersonalledelseProgram
