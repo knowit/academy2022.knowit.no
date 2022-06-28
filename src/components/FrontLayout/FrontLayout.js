eecd12003bb7f6172e3f473e41359c6e6c35dd1a
Thomas Malt <thomas@malt.no>
2022-06-28 10:41:58 +0200

    migrated to material-ui

--- src/components/FrontLayout/FrontLayout.js
+++ src/components/FrontLayout/FrontLayout.js
@@ -2,16 +2,15 @@ import React from 'react'
 import { MainHelmet } from '../MainHelmet'
 import HeaderMenu from '../AcademyHeaderMenu'
 import Footer from '../Footer/Footer'
-import * as css from './FrontLayout.module.scss'
 
 const FrontLayout = ({ children, data, sitemeta }) => {
   return (
-    <section className={css.frontpage}>
+    <>
       <MainHelmet metadata={data} />
       <HeaderMenu data={data} />
       <main>{children}</main>
       <Footer data={sitemeta} />
-    </section>
+    </>
   )
 }
 
