d793eae71a676e1627d1a458ed756f2b6262a9b4
Thomas Malt <thomas@malt.no>
2022-06-28 10:40:26 +0200

    Migrated to material ui

--- src/components/AboutAcademy/about.js
+++ src/components/AboutAcademy/about.js
@@ -1,19 +1,22 @@
-import React from "react"
-import * as css from "./about.module.scss"
+import * as React from 'react'
+import * as css from './about.module.scss'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
 
 const AboutAcademy = ({ content }) => {
   return (
-    <section id="about" className={css.aboutSection}>
-      <div className={css.gridContainer}>
-        <div className={css.frontMedal}>
-          <img src="/icons/medal.svg" alt="" />
-        </div>
-        <div
-          className={css.frontContent}
-          dangerouslySetInnerHTML={{ __html: content.html }}
-        />
-      </div>
-    </section>
+    <Container maxWidth="tablet">
+      <Box sx={{ textAlign: 'center', paddingBottom: 4 }}>
+        <Box sx={{ paddingTop: 12, paddingBottom: 4 }}>
+          <img
+            height="108"
+            src="/icons/medal.svg"
+            alt="Knowit Academy Medal Icon"
+          />
+        </Box>
+        <Box dangerouslySetInnerHTML={{ __html: content.html }} />
+      </Box>
+    </Container>
   )
 }
 
