309b8eda41f5334517c3ebd3cb581de352dd43d4
Thomas Malt <thomas@malt.no>
2022-06-29 13:49:21 +0200

    migrerte footer til material-ui

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,46 +1,70 @@
-import React from 'react'
-import Version from '../Version/Version'
-// import { repository } from '../../../package'
+import * as React from 'react'
 import * as css from './Footer.module.scss'
+import { useSiteMetadata } from 'hooks/useSiteMetadata'
+import { Container, Box, Link, Typography, Grid } from '@mui/material'
 
-const AcademyFooter = ({ data }) => {
-  if (
-    typeof data.site !== 'undefined' &&
-    typeof data.site.siteMetadata !== 'undefined'
-  ) {
-    data = data.site.siteMetadata
-  }
-  const { email } = data
+const AcademyFooter = () => {
+  const { email, version } = useSiteMetadata()
+
   return (
     <footer>
-      <section className={css.footerTop}>
-        <div className={css.gridContainer}>
-          <div className={css.footerTopContent}>
-            <img alt="" src="/icons/spark.svg"></img>
-            <h3>Spørsmål?</h3>
-            <p>
+      <section>
+        <Container maxWidth="tablet">
+          <Box pt={6} pb={5} className={css.footerTopContent}>
+            <Typography variant="h4" gutterBottom>
+              Spørsmål?
+            </Typography>
+            <img
+              alt="Decorative icon for exclamation."
+              src="/icons/spark.svg"
+            />
+            <Typography variant="body1" gutterBottom>
               Har du spørsmål om Knowit Academy? Send en epost til{' '}
               <a href={`mailto:${email}`}>{email}</a> så svarer vi så snart vi
               rekker.
-            </p>
-          </div>
-        </div>
+            </Typography>
+          </Box>
+        </Container>
       </section>
-      <section className={css.footerBottom}>
-        <div className={css.gridContainer}>
-          <div className={css.footerVersion}>
-            <Version></Version>
-          </div>
-          <div className={css.footerKnowit}>
-            <p>
-              <a href="/">Knowit Academy</a>
-            </p>
-            <a href="https://www.knowit.no/">
-              <img alt="" src="/icons/arrow.svg" height="22px" />
-              <img alt="" src="/assets/knowit_black.png" height="26px" />
-            </a>
-          </div>
-        </div>
+      <section>
+        <Container maxWidth="tablet">
+          <Grid container spacing={2} pt={3} pb={6} alignItems="flex-end">
+            <Grid item mobile={2}>
+              <Box pb={1} className={css.version}>
+                v{version}
+              </Box>
+            </Grid>
+            <Grid item mobile={10}>
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="flex-end"
+                alignItems="flex-end"
+                rowGap={1}
+                textAlign="right"
+              >
+                <Link
+                  href="/"
+                  variant="h3"
+                  color="#333333"
+                  fontFamily="ArialforKnowitRegular"
+                  display="block"
+                  underline="none"
+                >
+                  Knowit Academy
+                </Link>
+                <a
+                  href="https://www.knowit.no/"
+                  alt="Link til Knowits hjemmeside"
+                  className={css.knowitLink}
+                >
+                  <img alt="" src="/icons/arrow.svg" height="22px" />
+                  <img alt="" src="/assets/knowit_black.png" height="26px" />
+                </a>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
       </section>
     </footer>
   )
